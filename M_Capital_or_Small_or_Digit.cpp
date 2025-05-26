6d6d609fecb750b1ff3e7b45e8bd59826bdff905
Nasim <mdnasimhosen333@gmail.com>
2025-05-26 21:24:57 +0600

    Add solution for "M. Capital or Small or Digit" problem

---
+++ M_Capital_or_Small_or_Digit.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char c;
+    cin >> c;
+    if (c >= '0' && c <= '9')
+    {
+        cout << "IS DIGIT" << endl;
+    }
+    else
+    {
+        cout << "ALPHA" << endl;
+        if (c >= 'A' && c <= 'Z')
+        {
+            cout << "IS CAPITAL" << endl;
+        }
+        else if (c >= 'a' && c <= 'z')
+        {
+            cout << "IS SMALL" << endl;
+        }
+    }
+    return 0;
+}
