99c8122acced2ef059b7aee4ef462c1f71fbcf44
Nasim <mdnasimhosen333@gmail.com>
2025-05-29 23:34:44 +0600

    Add implementation for E. Gennady and Benq problem

---
+++ E_Gennady_and_Benq.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long x, y, z;
+    cin >> x >> y >> z;
+    if (x + y > z)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+    return 0;
+}
