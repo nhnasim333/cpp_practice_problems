7d39abcad1554609ca4296fa8c69b2cefb44ce99
Nasim <mdnasimhosen333@gmail.com>
2025-05-31 22:02:59 +0600

    Add implementation for A. Time Room problem

---
+++ A_Time_Room.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int Z, X;
+    cin >> Z >> X;
+
+    double t = (1.0 * Z) / X;
+
+    cout << fixed << setprecision(10) << t << endl;
+
+    return 0;
+}
