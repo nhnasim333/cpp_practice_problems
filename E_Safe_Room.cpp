45bc4e238530b709a503033a2d582faf29ba11e3
Nasim <mdnasimhosen333@gmail.com>
2025-05-31 22:17:00 +0600

    Refactor E. Safe Room implementation to calculate the sum of odd numbers in a range

--- E_Safe_Room.cpp
+++ E_Safe_Room.cpp
@@ -1,18 +1,24 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    int l, r;
-    cin >> l >> r;
-    int sum = 0;
-    for (int i = l; i <= r; i++)
+    int L, R;
+    cin >> L >> R;
+    if (L > R)
+        swap(L, R);
+
+    int first_odd = (L % 2 == 0) ? L + 1 : L;
+    int last_odd = (R % 2 == 0) ? R - 1 : R;
+
+    if (first_odd > last_odd)
     {
-        if (i % 2 != 0)
-        {
-            sum += i;
-        }
+        cout << 0 << endl;
+        return 0;
     }
+
+    int n = (last_odd - first_odd) / 2 + 1;
+    long long sum = 1LL * n * (first_odd + last_odd) / 2;
     cout << sum << endl;
     return 0;
 }
