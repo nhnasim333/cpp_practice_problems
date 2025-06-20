326996a05381ae9bad387aaac92fc47709afffb8
Nasim <mdnasimhosen333@gmail.com>
2025-06-20 15:16:29 +0600

    Add solution for A. Hulk problem

---
+++ A_Hulk.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    string result = "";
+    for (int i = 1; i <= n; ++i)
+    {
+        if (i % 2 == 1)
+        {
+            result += "I hate";
+        }
+        else
+        {
+            result += "I love";
+        }
+        if (i < n)
+        {
+            result += " that ";
+        }
+        else
+        {
+            result += " it";
+        }
+    }
+    cout << result << endl;
+    return 0;
+}
