f5364799ef6551f5c0ef5374a445e9ab38e7ef0c
Nasim <mdnasimhosen333@gmail.com>
2025-06-21 11:35:11 +0600

    Refactor F. Reversing solution to include time complexity comments and ensure proper formatting

--- F_Reversing.cpp
+++ F_Reversing.cpp
@@ -3,17 +3,19 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
+    int n;    // ----> O(1)
+    cin >> n; // ----> O(1)
     vector<int> arr(n);
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) // ----> O(n)
     {
         cin >> arr[i];
     }
-    reverse(arr.begin(), arr.end());
+    reverse(arr.begin(), arr.end()); // ----> O(n)
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
     return 0;
 }
+
+// Total time complexity: O(n) + O(n) + O(n) = O(n)
