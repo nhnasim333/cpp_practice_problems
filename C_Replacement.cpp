c09a7f5b86acab3e4d83f180cfa313f3e29739ed
Nasim <mdnasimhosen333@gmail.com>
2025-06-21 11:35:59 +0600

    Add time complexity comments to C. Replacement and F. Reversing solutions

--- C_Replacement.cpp
+++ C_Replacement.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
+    int n;    // ----> O(1)
+    cin >> n; // ----> O(1)
     vector<int> arr(n);
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) // ----> O(n)
     {
         cin >> arr[i];
     }
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) // ----> O(n)
     {
         if (arr[i] < 0)
         {
@@ -27,3 +27,5 @@ int main()
     }
     return 0;
 }
+
+// // Total time complexity: O(n) + O(n) = O(n)
--- F_Reversing.cpp
+++ F_Reversing.cpp
@@ -11,7 +11,7 @@ int main()
         cin >> arr[i];
     }
     reverse(arr.begin(), arr.end()); // ----> O(n)
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)      // ----> O(n)
     {
         cout << arr[i] << " ";
     }
