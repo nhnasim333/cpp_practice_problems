6930c4b5178a164ba8de2c7c0d1f19000ae86405
Nasim <mdnasimhosen333@gmail.com>
2025-06-21 11:40:48 +0600

    Add initial implementation for D. Counting Elements problem

---
+++ D_Counting_Elements.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[i] + 1 == arr[j])
+            {
+                count++;
+                break;
+            }
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
