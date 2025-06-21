88f0f60ccbb4dfbfba802c6b9cf7fff099086b5e
Nasim <mdnasimhosen333@gmail.com>
2025-06-21 11:29:28 +0600

    Refactor F. Reversing solution to use vector instead of array and add problem metadata

--- F_Reversing.cpp
+++ F_Reversing.cpp
@@ -5,15 +5,12 @@ int main()
 {
     int n;
     cin >> n;
-    int arr[n];
+    vector<int> arr(n);
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
-    for (int i = 0; i < n / 2; i++)
-    {
-        swap(arr[i], arr[n - i - 1]);
-    }
+    reverse(arr.begin(), arr.end());
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
