773e79f83ba0866fe63b4be450be35fe9ba88870
Nasim <mdnasimhosen333@gmail.com>
2025-07-03 01:22:34 +0600

    Add initial implementation for A. Game With Sticks problem

---
+++ A_Game_With_Sticks.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int moves = min(n, m);
+    if (moves % 2 == 1)
+        cout << "Akshat" << endl;
+    else
+        cout << "Malvika" << endl;
+    return 0;
+}
