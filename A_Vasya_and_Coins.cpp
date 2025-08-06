755120e5a1ac20f60830432754bd9492327bf876
Nasim <mdnasimhosen333@gmail.com>
2025-08-06 19:43:13 +0600

    Add initial implementation for A. Vasya and Coins problem

---
+++ A_Vasya_and_Coins.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long a, b;
+        cin >> a >> b;
+        if (a == 0)
+            cout << 1 << endl;
+        else
+            cout << a + b * 2 + 1 << endl;
+    }
+    return 0;
+}
