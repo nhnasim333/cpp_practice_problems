bf0256f6aedd9797dd4dc2b7a8d7f15eff19051d
Nasim <mdnasimhosen333@gmail.com>
2025-08-08 20:26:45 +0600

    Add initial implementation for A. QAQ problem

---
+++ A_QAQ.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    int count = 0;
+    int n = s.size();
+    for (int i = 0; i < n; ++i)
+    {
+        if (s[i] == 'Q')
+        {
+            for (int j = i + 1; j < n; ++j)
+            {
+                if (s[j] == 'A')
+                {
+                    for (int k = j + 1; k < n; ++k)
+                    {
+                        if (s[k] == 'Q')
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
