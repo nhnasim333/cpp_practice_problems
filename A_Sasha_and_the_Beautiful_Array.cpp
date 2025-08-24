c441e15fe551f632f668695a85fe14c7f4f67389
Nasim <mdnasimhosen333@gmail.com>
2025-08-24 21:19:10 +0600

    Add initial implementation for A. Sasha and the Beautiful Array problem

---
+++ A_Sasha_and_the_Beautiful_Array.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<long long> a(n);
+        for (int i = 0; i < n; ++i)
+            cin >> a[i];
+        long long mx = *max_element(a.begin(), a.end());
+        long long mn = *min_element(a.begin(), a.end());
+        cout << mx - mn << endl;
+    }
+    return 0;
+}
