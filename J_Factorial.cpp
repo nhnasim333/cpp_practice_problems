50f58cd4bd154648240068d874b727775f09555e
Nasim <mdnasimhosen333@gmail.com>
2025-09-09 10:52:56 +0600

    Add initial implementation for J. Factorial problem using recursion

---
+++ J_Factorial.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long int fac(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    return n * fac(n - 1);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << fac(n) << endl;
+
+    return 0;
+}
