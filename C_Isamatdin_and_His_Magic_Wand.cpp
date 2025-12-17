7e0c7fea057b60d8e439d7add7c970401c356abd
Nasim <mdnasimhosen333@gmail.com>
2025-12-17 21:27:11 +0600

    Add initial implementation for C. Isamatdin and His Magic Wand problem

---
+++ C_Isamatdin_and_His_Magic_Wand.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    if (!(cin >> t))
+        return 0;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<long long> a(n);
+        bool hasOdd = false, hasEven = false;
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> a[i];
+            if (a[i] % 2 == 0)
+                hasEven = true;
+            else
+                hasOdd = true;
+        }
+        if (hasOdd && hasEven)
+        {
+            vector<long long> b = a;
+            sort(b.begin(), b.end());
+            for (int i = 0; i < n; ++i)
+            {
+                cout << b[i] << (i + 1 == n ? '\n' : ' ');
+            }
+        }
+        else
+        {
+            for (int i = 0; i < n; ++i)
+            {
+                cout << a[i] << (i + 1 == n ? '\n' : ' ');
+            }
+        }
+    }
+    return 0;
+}
