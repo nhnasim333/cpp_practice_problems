f07de404734227fe74b2f87517ebd40a0c3e7ce9
Nasim <mdnasimhosen333@gmail.com>
2026-01-01 19:31:06 +0600

    Refactor A. Perfect Permutation implementation for multiple test cases and update problem details

--- A_Perfect_Permutation.cpp
+++ A_Perfect_Permutation.cpp
@@ -1,16 +1,39 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
-    if (n % 2 != 0) {
-        cout << -1 << endl;
-    } else {
-        for (int i = 1; i <= n; i += 2) {
-            cout << i + 1 << " " << i << " ";
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> p(n + 1);
+
+        for (int i = 1; i <= n; i += 2)
+        {
+            if (i + 1 <= n)
+            {
+                p[i] = i + 1;
+                p[i + 1] = i;
+            }
+            else
+            {
+                p[i] = i;
+            }
         }
-        cout << endl;
+
+        for (int i = 1; i <= n; i++)
+        {
+            cout << p[i];
+            if (i < n)
+                cout << " ";
+        }
+        cout << "\n";
     }
+
     return 0;
 }
