52e7ffa95d800e38d3657a8419c63758cad8b857
Nasim <mdnasimhosen333@gmail.com>
2026-01-01 19:53:35 +0600

    Add implementation for A. Two Groups problem from Codeforces Round 832 (Div. 2)

---
+++ A_Two_Groups.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<long long> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        long long sum_positive = 0;
+        long long sum_negative = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] > 0)
+            {
+                sum_positive += a[i];
+            }
+            else if (a[i] < 0)
+            {
+                sum_negative += a[i];
+            }
+            // zeros don't affect the result, can go anywhere
+        }
+
+        // Maximum difference is when we separate positives and negatives
+        long long result = abs(sum_positive) - abs(sum_negative);
+        result = abs(result);
+
+        cout << result << endl;
+    }
+
+    return 0;
+}
